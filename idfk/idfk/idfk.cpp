83a210ea7619f9e2b9d6eebaa992c416aa3d5c18
AGKostov23 <AGKostov23@codingburgas.bg>
2025-03-05 17:35:00 +0000

    Database Concept

---
+++ idfk/idfk/idfk.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include "Test1.h"
+using namespace std;
+
+int main() {
+    string filename = "C:/ladaad/DB.txt";
+    takeQuizAndSaveScore(filename);
+    return 0;
+}

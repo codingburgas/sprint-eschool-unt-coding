c7bf0429cd0c78eef9c6a64270f573fa0099654f
DDMeshkov23 <DDMeshkov23@codingburgas.bg>
2025-03-15 14:49:36 +0200

    Create scores.h and scores.cpp and enhance main.cpp

--- FluentCheck/FluentCheck/main.cpp
+++ FluentCheck/FluentCheck/main.cpp
@@ -2,8 +2,18 @@
 #include <fstream>
 #include "testBeginner.h"
 #include "options.h"
+#include "scores.h"
 using namespace std;
 
 int main() {
     MainMenu();
+
+    int scores[100], count;
+    int topScores[3];
+
+    readScores("BeginnerTestDB.txt", scores, count);
+    findTopScores(scores, count, topScores);
+    displayTopScores(topScores);
+
+    return 0;
 }
